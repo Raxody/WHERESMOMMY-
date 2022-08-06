a0d13149ab7086d87d08c85d355919b535764bbd
JuanLopez <juancho_lopez6@hotmail.com>
2022-08-05 23:58:57 -0500

    Modulo funcionario y recursos implementado en su totalidad

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,5 @@
 import controlador.Controlador;
 import infraestructura.Conexion;
-import jdk.swing.interop.SwingInterOpUtils;
-import modelo.entidad.Adopcion;
-import modelo.entidad.Funcionario;
-import modelo.entidad.Huerfano;
-import modelo.entidad.Recursos;
-import modelo.servicio.*;
-
-import javax.swing.*;
 
 public class Principal {
 
@@ -18,30 +10,6 @@ public class Principal {
         Controlador controlador = new Controlador();
 
         controlador.iniciar();
-/*
-        IRecurso recurso = new ServicioRecurso();
-
-        System.out.println(recurso.crearRecursos(new Recursos("lapices",200)));;
-
-        System.out.println(recurso.actualizarRecursos(new Recursos("sillassss",20)));;
-
-        recurso.listarRecursos().forEach(adopcion -> System.out.println(adopcion.toString()));
-
-
-        System.out.println(recurso.buscarRecursos("lapices").toString());
-
-        System.out.println(recurso.eliminarRecursos("lapices"));
-
-        IFuncionarios funcionarios = new ServicioFuncionario();
-
-        System.out.println(funcionarios.crearFuncionarios(new Funcionario(1234567892L,"Andres","Profesor",25)));;
-
-
-        System.out.println(funcionarios.actualizarFuncionarios(new Funcionario(1234567892L,"Andresito","Profesorcito",26)));;
-
-        System.out.println(funcionarios.eliminarFuncionarios(1234567892L));
-
-        funcionarios.listarFuncionarios().forEach(adopcion -> System.out.println(adopcion.toString()));*/
 
 
     }
